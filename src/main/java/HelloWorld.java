efc64768beb3f25c80e6db0796abc0ae3636d953
silktreesolutions <joel.r.julian0408@gmail.com>
2019-03-21 10:15:49 +0530

    Added new feature in feature2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Good Morning");
         System.out.println("Here is an additional feature");
+        System.out.println("This is feature1 line1");
+        System.out.println("This is feature1 line2");
+        System.out.println("This is feature2 line1");
+        System.out.println("This is feature2 line2");
     }
 }
